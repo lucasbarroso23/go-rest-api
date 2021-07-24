2f5725c21d6c68bef24314739be0aaf95dc8c5c5
Lucas Barroso <lucasbarroso23@gmail.com>
2021-07-24 17:08:34 -0300

    productHandler

--- server.go
+++ server.go
@@ -1,13 +1,79 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
+
+type Product struct {
+	Name  string  `json:"name"`
+	Price float64 `json: "price"`
+}
+
+type Products []Product
+
+type productHandler struct {
+	sync.Mutex
+	products Products
+}
+
+func (ph *productHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		ph.get(w, r)
+	case "POST":
+		ph.post(w, r)
+	case "PUT", "PATCH":
+		ph.put(w, r)
+	case "DELETE":
+		ph.delete(w, r)
+	default:
+		respondWithError(w, http.StatusMethodNotAllowed, "invalid method")
+	}
+}
+
+func (ph *productHandler) get(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from get")
+}
+func (ph *productHandler) post(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from post")
+}
+func (ph *productHandler) put(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from put")
+}
+func (ph *productHandler) delete(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from delete")
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJSON(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, data interface{}) {
+	response, _ := json.Marshal(data)
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func newProductHandler() *productHandler {
+	return &productHandler{
+		products: Products{
+			Product{"Shoes", 25.00},
+			Product{"Webcam", 50.00},
+			Product{"Mic", 20.00},
+		},
+	}
+}
 
 func main() {
 	port := ":8080"
+	ph := newProductHandler()
+	http.Handle("/products", ph)
+	http.Handle("products/", ph)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
