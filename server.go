aa4e1d3b37f10747df879baa48282b539aee2ae4
Lucas Barroso <lucasbarroso23@gmail.com>
2021-07-24 16:00:29 -0300

    server setup

---
+++ server.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	port := ":8080"
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World")
+	})
+	log.Fatal(http.ListenAndServe(port, nil))
+}
